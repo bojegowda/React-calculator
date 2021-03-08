22e440b7f46cb2527bd04665f2c20a5377f91dda
bojegowda671@gmail.com <bojegowda671@gmail.com>
2021-03-08 22:58:55 +0530

    created keyboard, result components

---
+++ calculator/src/components/Result.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Result extends React.Component {
+  render() {
+    let { result } = this.props;
+    return (
+      <div className="result">
+        <p> {result} </p>{" "}
+      </div>
+    );
+  }
+}
+
+export default Result;
